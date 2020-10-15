b36a4dfccc47c99fb6e756c67de128f6f6b7fee4
mikitsuba <tsubasa4997@gmail.com>
2020-10-15 23:38:28 +0900

    php課題とりあえず

---
+++ php01/kansu.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <style>
+        .menu {
+            background-color: #2FA6E9;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="menu">
+        <h3>menu</h3>
+        <ul>
+            <li>組み込み関数を知る</li>
+            <li>if文と文と関数を組み合わせておみくじを作る</li>
+            <li>(演習)自由におみくじをアレンジする</li>
+            <li><a href="index.php">index.php</a></li>
+        </ul>
+    </div>
+    <!-- ここから -->
+
+    // 日付表示
+
+    // 文字置き換え
+    $phoneNum = '090-0000-0000 ';
+
+    // ランダムな数字を表示する(rand)
+
+
+    <!-- ここまで -->
+</body>
+
+</html>
---
+++ php01/write.php
@@ -0,0 +1,26 @@
+<?php
+// ファイルに書き込み
+
+//文字作成
+
+?>
+
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>File書き込み</title>
+</head>
+
+<body>
+
+    <h1>書き込みしました。</h1>
+    <h2>./data/data.txt を確認しましょう！</h2>
+
+    <ul>
+        <li><a href="input.php">戻る</a></li>
+    </ul>
+</body>
+
+</html>
